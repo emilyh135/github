f8f4f8374a5b5b2866c4c35b07e8da409da365cc
emilyh135 <jmtzadrian7@gmail.com>
2025-07-03 13:55:50 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express'); 
+// Import the Express library
+const app = express(); // Create an Express application
+
+const port = 3000; // Define the port number for the server
+
+app.get('/', (req, res) => { // Define a route for the root URL
+  res.send('Flower Shop'); // Send a response to the client
+});
+
+app.listen(port, ()  => { // Start the server and listen on the specified port
+  console.log(`Server is running on http://localhost:${port}`); // Log a message to the console
+});
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+}
+
+app.get('/home', (req, res) =>
+  res.sendFile(__dirname + '/home.html');
+}
+
+app.get('/about', (req, res) => {
+  res.sendFile(__dirname + '/about.html');
+}
